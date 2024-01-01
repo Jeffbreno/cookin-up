2a0c47ab7e930f42da2ac158fc356e6dbee1813c
Jefferson Lopes <jeffbreno@gmail.com>
2024-01-01 18:57:23 -0300

    01012024_1857

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
-import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import SelecionarIngredientes from "./SelecionarIngredientes.vue";
+import Tag from "./Tag.vue";
 
 export default {
   data() {
@@ -7,7 +8,7 @@ export default {
       ingredientes: ["Alho", "Manteiga", "Orégano", "Tomate"],
     };
   },
-  components: { SelecionarIngredientes }
+  components: { SelecionarIngredientes, Tag },
 };
 </script>
 
@@ -17,7 +18,9 @@ export default {
       <span class="subtitulo-lg sua-lista-texto"> Sua lista: </span>
 
       <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">{{ ingrediente }}</li>
+        <li v-for="ingrediente in ingredientes" :key="ingrediente">
+          <Tag :texto="ingrediente" ativa/>
+        </li>
       </ul>
 
       <p v-else="ingredientes.length" class="paragrafo lista-vazia">
@@ -54,18 +57,6 @@ export default {
   justify-content: center;
   gap: 1rem 1.5rem;
   flex-wrap: wrap;
-}
-
-.ingrediente {
-  display: inline-block;
-  border-radius: 0.5rem;
-  min-width: 4.25rem;
-  padding: 0.5rem;
-  text-align: center;
-  transition: 0.2s;
-  color: var(--creme, #fffaf3);
-  background: var(--coral, #f0633c);
-  font-weight: 700;
 }
 
 .lista-vazia {
